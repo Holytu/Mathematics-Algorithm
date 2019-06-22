d999707debc1ab335cd4e06cc346a5a6b829c8fd
Holytu <nthuxy12345@gmail.com>
2019-06-22 22:43:17 +0800

    Update Power(DivideAndConquer).cpp

--- Power(DivideAndConquer).cpp
+++ Power(DivideAndConquer).cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 using namespace std;
-int pow(int a, int b)
+int powOne(int a, int b)
 {
-    if(b == 1)
-        return a;
+    if(b == 0)
+        return 1;
 
-    int x = pow(a, b / 2);
+    int x = powOne(a, b / 2);
 
     // b & 1 : check b is odd or even. If b & 1 == 1, it's a odd number.
     return b & 1 ? a * x * x : x * x;
 }
+int powTwo(int a, int b)
+{
+    if(b == 0)
+        return 1;
+
+    int n = 1;
+    for( ; b ; b>>=1)
+    {
+        if(b & 1)
+            n = n * a;
+
+        a = a * a;
+    }
+    return n;
+}
 int main()
 {
-    cout<<pow(7,3)<<endl;
+    cout<<powTwo(7,3)<<endl;
     return 0;
 }
